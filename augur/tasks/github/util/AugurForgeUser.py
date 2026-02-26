8e0b17bcffe34ff642986df3f4804cac083ce130
Adrian Edwards <adredwar@redhat.com>
2026-02-26 16:45:56 -0500

    add augur forge user first pass class/fields

---
+++ augur/tasks/github/util/AugurForgeUser.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+# from .util import AugurPlatformType
+from datetime import datetime
+
+@dataclass
+class AugurForgeUser:
+    """Data class to act as an adapter layer between augur
+    and different platforms representations of a user"""
+    
+    identifier: int
+    username: str
+    name: str
+    company: str
+    location: str
+    email: str
+    user_type: str
+    is_admin: bool
+    created_at: datetime
+    updated_at: datetime
+    # forgeType: AugurPlatformType
